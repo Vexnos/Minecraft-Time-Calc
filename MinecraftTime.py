fb1e43894adf82d746355915b81e8304e5cecaf1
Vexnos <88638499+Vexnos@users.noreply.github.com>
2023-09-02 12:33:35 +1200

    Made the program loopable

--- MinecraftTime.py
+++ MinecraftTime.py
@@ -15,39 +15,57 @@ def timeCalc(n, a):
 
 #-------Main-Routine-------
 if __name__ == "__main__":
-    validating_time = True
-    validating_rotations = True
+    looping = True
+    while looping:
 
-    # Validate the time
-    while validating_time:
+        validating_time = True
+        validating_rotations = True
 
-        # Attempt
-        try:
-            time = int(input("Enter your desired time (in ticks): "))
+        print('''
+            #1 = Full Moon
+            #2 = Waining Gibbous Moon
+            #3 = Last Quarter Moon
+            #4 = Waining Crescent Moon
+            #5 = New Moon
+            #6 = Waxing Crescent Moon
+            #7 = First Quarter Moon
+            #8 = Waxing Gibbous Moon
+            #9 = Full Moon
+        ''')
 
-        # Failure
-        except ValueError:
-            print("Invalid value entered, please try again")
-            continue
+        # Validate the time
+        while validating_time:
 
-        # Success
-        validating_time = False
+            # Attempt
+            try:
+                time = int(input("Enter your desired time (in ticks): "))
 
-    # Validate the rotations
-    while validating_rotations:
+            # Failure
+            except ValueError:
+                print("Invalid value entered, please try again")
+                continue
 
-        # Attempt
-        try:
-            rotations = int(input("\nEnter the amount of planetary rotations: "))
+            # Success
+            validating_time = False
 
-        # Failure
-        except ValueError:
-            print("Invalid value entered, please try again")
-            continue
+        # Validate the rotations
+        while validating_rotations:
 
-        # Success
-        validating_rotations = False
+            # Attempt
+            try:
+                rotations = int(input("\nEnter the amount of planetary rotations: "))
 
-    # Run the Calulator Function and Print the result
-    result = timeCalc(time, rotations)
-    print(f"\n{result}")
+            # Failure
+            except ValueError:
+                print("Invalid value entered, please try again")
+                continue
+
+            # Success
+            validating_rotations = False
+
+        # Run the Calulator Function and Print the result
+        result = timeCalc(time, rotations)
+        print(f"\n{result}")
+
+        loop_again = input("\nLoop again? (y/n): ")
+        looping = loop_again.lower().startswith("y")
